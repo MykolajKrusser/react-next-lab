b9b8ddbe9a1b3a7598410f6060dc26ce1ae420a9
mkrusser <mkrusser@goldencomm.com>
2022-03-09 13:32:08 +0100

    router research

---
+++ web-app/pages/clients/[id]/index.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+
+function SelectedClientProjectPage(){
+  const router = useRouter();
+  console.log(router.query)
+  return(
+    <div>
+      <h1>Project page for a specific project for a selected client</h1>
+    </div>
+  )
+}
+
+export default SelectedClientProjectPage;
---
+++ web-app/pages/portfolio/list.js
@@ -0,0 +1,9 @@
+function List() {
+  return(
+    <div>
+      <h1>list page</h1>
+    </div>
+  )
+}
+
+export default List;
