ba6c4a09d890364032bc0db5e26de44d875368f3
mkrusser <mkrusser@goldencomm.com>
2022-03-09 14:35:09 +0100

    site navigation

--- web-app/pages/clients/[id]/index.js
+++ web-app/pages/clients/[id]/index.js
@@ -4,9 +4,20 @@ import { useRouter } from 'next/router';
 function SelectedClientProjectPage(){
   const router = useRouter();
   console.log(router.query)
+
+  function loadProjectHandler(){
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'a' }
+    })
+  }
+
   return(
     <div>
       <h1>Project page for a specific project for a selected client</h1>
+      <button onClick={loadProjectHandler}>
+        Load project A
+      </button>
     </div>
   )
 }
--- web-app/pages/clients/index.js
+++ web-app/pages/clients/index.js
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+
 function ClientsPage(){
+  const clients =[
+    {id: 'max', name: 'Maxx'},
+    {id: 'bax', name: 'Baxx'}
+  ]
+
   return(
     <div>
       <h1>Clients page</h1>
+      <ul>
+        {
+          clients.map(client => {
+            return (
+              <li key={client.id}>
+                <Link href={{
+                  pathname: '/clients/[id]',
+                  query: { id: client.id }
+                }}>{client.name}</Link>
+              </li>
+            )
+          })
+        }
+      </ul>
+
     </div>
   )
 }
