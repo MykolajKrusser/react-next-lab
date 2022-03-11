20aefa2440acf4bf83a9c17a5c46296ae5ce2a31
mkrusser <mkrusser@goldencomm.com>
2022-03-11 13:03:26 +0100

    added events filter

--- web-app/pages/events/index.js
+++ web-app/pages/events/index.js
@@ -1,10 +1,23 @@
+import {Fragment} from "react";
+import {getAllEvents} from "../../dummy_data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import {useRouter} from "next/router";
+
 function Events(){
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month){
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
+
   return(
-    <div>
-      <h1>
-        All Events
-      </h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventHandler} />
+      <EventList items={events}/>
+    </Fragment>
   )
 }
 
